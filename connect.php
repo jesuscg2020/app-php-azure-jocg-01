4dd5fa5f3146c31494e054e6cf2a219f5f9bae6f
jesuscg2020 <84473561+jesuscg2020@users.noreply.github.com>
2021-11-05 17:14:43 -0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $port = 1433;
 $serverName = "tcp:myserver-jocg-01.database.windows.net," . $port;
-$database = "AdventureWorks (myserver-jocg-01/AdventureWorks)";
+$database = "AdventureWorks";
 $userName = "Student";
 $password = "Pa55w.rd";
 
